e4ab5d0d571be1496aef8eab457216a9390f1c47
m-hayabusa <s.hashimoto@hs-sh.net>
2020-11-09 23:08:03 +0900

    Added indicator LED

--- keyboards/jismini/jismini.c
+++ keyboards/jismini/jismini.c
@@ -41,10 +41,13 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 
     return process_record_user(keycode, record);
 }
-
+*/
 bool led_update_kb(led_t led_state) {
     // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
 
+    writePin(D5, !led_state.num_lock);
+    writePin(B0, !led_state.caps_lock);
+
     return led_update_user(led_state);
 }
-*/
+
